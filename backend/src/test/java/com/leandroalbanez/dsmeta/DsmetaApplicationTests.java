299e08ed3575ca1d80a3157052a4aab9535dfafd
Leandro Rezende Albanez <leandrorezende89@gmail.com>
2022-07-16 18:39:08 -0300

    Project created

---
+++ backend/src/test/java/com/leandroalbanez/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroalbanez.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
