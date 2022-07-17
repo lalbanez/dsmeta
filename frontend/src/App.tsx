8ff0a8e63b1998f503cb8acf2b5b99bddd791aa3
Leandro Rezende Albanez <leandrorezende89@gmail.com>
2022-07-16 22:01:47 -0300

    Others Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
-return(
-  <>
-    <h1>Olá mundo!</h1>
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
-    <NotificationButton />
-  </>
-)
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
